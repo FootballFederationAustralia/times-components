283fdb018fe09b1565eca29859547e82b20eea53
Beau <beau@domesticcat.com.au>
2018-04-09 13:01:16 +1000

    fix brightcove video from not picking up merged js

--- packages/brightcove-video/src/brightcove-video.js
+++ packages/brightcove-video/src/brightcove-video.js
@@ -1,31 +1,40 @@
-import React, { Component } from "react";
-import { View, TouchableWithoutFeedback, NativeModules } from "react-native";
-import PropTypes from "prop-types";
+import React, { Component } from 'react'
+import {
+  View,
+  TouchableWithoutFeedback,
+  NativeModules,
+  NativeEventEmitter,
+} from 'react-native'
+import PropTypes from 'prop-types'
 
-import Player from "./brightcove-player";
-import Splash from "./splash";
-import VideoError from "./error";
+import Player from './brightcove-player'
+import Splash from './splash'
+import VideoError from './error'
+
+const { RNTBrightcoveVideoEventSignal } = NativeModules
+
+const videoSignal = new NativeEventEmitter(RNTBrightcoveVideoEventSignal)
 
 const BrightcoveFullscreenPlayerModule =
-  NativeModules.BrightcoveFullscreenPlayer;
+  NativeModules.BrightcoveFullscreenPlayer
 
 class BrightcoveVideo extends Component {
   static getBrightcoveFullscreenPlayerModule() {
-    return BrightcoveFullscreenPlayerModule;
+    return BrightcoveFullscreenPlayerModule
   }
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       isLaunched: props.autoplay,
-      error: null
-    };
+      error: null,
+    }
 
-    this.play = this.play.bind(this);
-    this.reset = this.reset.bind(this);
-    this.handleFinish = this.handleFinish.bind(this);
-    this.handleError = this.handleError.bind(this);
+    this.play = this.play.bind(this)
+    this.reset = this.reset.bind(this)
+    this.handleFinish = this.handleFinish.bind(this)
+    this.handleError = this.handleError.bind(this)
   }
 
   // specifically check if is launched has changed and block update if it has not;
@@ -35,70 +44,93 @@ class BrightcoveVideo extends Component {
       nextState.isLaunched !== this.state.isLaunched ||
       nextState.error !== this.state.error ||
       nextProps !== this.props
-    );
+    )
+  }
+  componentWillUnmount() {
+    this.removeSignals()
+  }
+
+  setupSignals() {
+    if (!this.state.onPlay && !this.state.onClose) {
+      const onPlay = videoSignal.addListener('onPlayNotification', reminder =>
+        this.props.onPlay(),
+      )
+      const onClose = videoSignal.addListener('onCloseNotification', reminder =>
+        this.props.onFinish(),
+      )
+      this.setState({ ...this.state, onPlay, onClose })
+    }
+  }
+
+  removeSignals() {
+    if (this.state.onPlay && this.state.onClose) {
+      this.state.onPlay.remove()
+      this.state.onClose.remove()
+    }
   }
 
   play() {
-    const nativeModule = BrightcoveVideo.getBrightcoveFullscreenPlayerModule();
+    const nativeModule = BrightcoveVideo.getBrightcoveFullscreenPlayerModule()
 
     if (nativeModule && this.props.directToFullscreen) {
+      this.setupSignals()
       nativeModule.playVideo({
         accountId: this.props.accountId,
         videoId: this.props.videoId,
-        policyKey: this.props.policyKey
-      });
+        policyKey: this.props.policyKey,
+      })
     } else {
       if (this.playerRef) {
-        this.playerRef.play();
+        this.playerRef.play()
       }
 
-      this.setState({ isLaunched: true });
+      this.setState({ isLaunched: true })
     }
   }
 
   pause() {
     if (this.playerRef) {
-      this.playerRef.pause();
+      this.playerRef.pause()
     }
   }
 
   reset() {
-    this.setState({ isLaunched: false, error: null });
+    this.setState({ isLaunched: false, error: null })
   }
 
   handleFinish() {
     if (this.props.resetOnFinish) {
-      this.reset();
+      this.reset()
     }
 
-    this.props.onFinish();
+    this.props.onFinish()
   }
 
   handleError(error) {
-    this.setState({ error });
+    this.setState({ error })
 
-    this.props.onError(error);
+    this.props.onError(error)
   }
 
   render() {
-    this.playerRef = null;
+    this.playerRef = null
 
     if (this.state.error) {
-      return <VideoError {...this.props} onReset={this.reset} />;
+      return <VideoError {...this.props} onReset={this.reset} />
     }
 
     if (this.state.isLaunched) {
       return (
         <Player
           ref={ref => {
-            this.playerRef = ref;
+            this.playerRef = ref
           }}
           {...this.props}
           onError={this.handleError}
           onFinish={this.handleFinish}
           autoplay
         />
-      );
+      )
     }
 
     return (
@@ -107,7 +139,7 @@ class BrightcoveVideo extends Component {
           <Splash {...this.props} />
         </View>
       </TouchableWithoutFeedback>
-    );
+    )
   }
 }
 
@@ -115,16 +147,16 @@ BrightcoveVideo.propTypes = {
   resetOnFinish: PropTypes.bool,
   directToFullscreen: PropTypes.bool,
   ...Splash.propTypes,
-  ...Player.propTypes
-};
+  ...Player.propTypes,
+}
 
 BrightcoveVideo.defaultProps = Object.assign(
   {
     resetOnFinish: false,
-    directToFullscreen: false
+    directToFullscreen: false,
   },
   Splash.defaultProps,
-  Player.defaultProps
-);
+  Player.defaultProps,
+)
 
-export default BrightcoveVideo;
+export default BrightcoveVideo
